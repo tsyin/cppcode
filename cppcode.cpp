6cb8393a55a72a0c770efe5b02cafcaf973fdb25
tsyin_mobi <1115653102@qq.com>
2022-04-20 23:29:26 +0800

    sss

--- cppcode.cpp
+++ cppcode.cpp
@@ -1,23 +1,8 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
-    int sum[100] = {0};
-    int i = 0;
-    int a = 0,b = 0,temp = 0;
-    while(cin >> temp)
-    {
-        i++;
-        if(i%2==0){
-           b = temp; 
-           sum[i/2] = a + b;
-           cout << endl;
-        }
-        else
-            a = temp; 
-
-    }
-    for(int j = 0;j++;j<i/2)
-        cout << sum[j]<<endl;
+    cout << 1234/2000;
     return 0;
 }
